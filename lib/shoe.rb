ff07ea589e6a93b6398fd3509069e27e152299b8
Abel Kalum <chumbabel@gmail.com>
2020-01-30 15:37:18 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,18 @@
 # shoe.rb
  
 class Shoe
-  
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
   
   
  
